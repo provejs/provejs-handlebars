5372c17f8fd4fe73f472f6af8d82fac9d675a6b3
dhollenbeck <dan@hollenbecks.com>
2018-03-27 16:25:23 -0500

    more tests

--- test/helper_named_params.js
+++ test/helper_named_params.js
@@ -79,4 +79,46 @@ describe('Linting helper named parameters', function () {
 		var actual = Linter(config, ast);
 		Assert.equal(actual.length, 0);
 	});
+	it('missing second optional helper named parameter should NOT generate error', function () {
+		var html = "{{helper1 param1='foo'}}";
+		var config = {
+			helpers: {
+				helper1: {
+					params: [{
+						name: 'param1',
+						hashed: true,
+						formats: ['string', 'variable'],
+						required: true
+					}, {
+						name: 'param2',
+						hashed: true,
+						formats: ['string', 'variable'],
+						required: false
+					}]
+				}
+			}
+		};
+		var ast = Handlebars.parse(html);
+		var actual = Linter(config, ast);
+		Assert.equal(actual.length, 0);
+	});
+	it('helper named parameter with wrong value format should generate error', function () {
+		var html = "{{helper1 param1=42}}";
+		var config = {
+			helpers: {
+				helper1: {
+					params: [{
+						name: 'param1',
+						hashed: true,
+						formats: ['string'],
+						required: true
+					}]
+				}
+			}
+		};
+		var ast = Handlebars.parse(html);
+		var actual = Linter(config, ast);
+		Assert.equal(actual.length, 1);
+	});
+	it('extra helper named parameter should generate warning');
 });
