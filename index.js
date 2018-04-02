7d441d9ae19cbb2af78d5bf3fc3b40b40794263e
dhollenbeck <dan@hollenbecks.com>
2018-04-02 13:01:38 -0500

    bug fixes regarding empty configs

--- index.js
+++ index.js
@@ -95,6 +95,9 @@ function lintHelperParam(astHelper, rule, ruleKey) {
 }
 
 function lintHelper(astHelper, objRules) {
+
+	if (!objRules) return;
+
 	var error;
 	var params = objRules.params;
 
@@ -116,7 +119,7 @@ function lintHelper(astHelper, objRules) {
 function lintHelpers(helpers, rules) {
 	var errors = [];
 	helpers.forEach(function(helper) {
-		var config = rules.helpers[helper.name];
+		var config = rules.helpers && rules.helpers[helper.name];
 		var err = lintHelper(helper, config);
 		if (err) errors.push(err);
 	});
---
+++ test/helpers/config.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var Assert = require('assert');
+var Linter = require('../../index').linter;
+
+describe('Linting helper params bogus configs', function () {
+	it('no config and with helper', function () {
+		var html = '{{helper1 param1=42}}';
+		var actual = Linter(html);
+		Assert.equal(actual.length, 0);
+	});
+	it('empty config', function () {
+		var html = '{{helper1 param1=42}}';
+		var actual = Linter(html, {});
+		Assert.equal(actual.length, 0);
+	});
+	it('misconfig helpers array', function () {
+		var html = '{{helper1 param1=42}}';
+
+		var actual = Linter(html, {helpers: []});
+		Assert.equal(actual.length, 0);
+	});
+	it('misconfig helpers helper array', function () {
+		var html = '{{helper1 param1=42}}';
+		var config = {
+			helpers: {
+				helper1: []
+			}
+		};
+		var actual = Linter(html, config);
+		Assert.equal(actual.length, 0);
+	});
+	it('misconfig helpers helper object', function () {
+		var html = '{{helper1 param1=42}}';
+		var config = {
+			helpers: {
+				helper1: {}
+			}
+		};
+		var actual = Linter(html, config);
+		Assert.equal(actual.length, 0);
+	});
+});
